aff76be940271b0531b4ee6f79fccf98ee3f4a45
Yatin Mahajan <yatin.mahajan@seagate.com>
2021-12-22 04:52:40 -0700

    Disable DI check for degraded mode read by default.
    Signed-off-by: Yatin Mahajan <yatin.mahajan@seagate.com>

--- motr/io_req.c
+++ motr/io_req.c
@@ -1325,7 +1325,8 @@ static int ioreq_application_data_copy(struct m0_op_io *ioo,
 		 * skip checksum verification during degraded I/O
 		 */
 		if (ioreq_sm_state(ioo) != IRS_DEGRADED_READING &&
-		    ioo->ioo_attr.ov_vec.v_nr &&
+		    ioo->ioo_attr.ov_vec.v_nr != 0 && !ioo->ioo_dgmode_io_sent &&
+		    !is_parity_verify_mode(m0__op_instance(&ioo->ioo_oo.oo_oc.oc_op)) &&
 		    !verify_checksum(ioo)) {
 			return M0_RC(-EIO);
 		}
